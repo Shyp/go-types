7f12c868367aec3ed10ddf43ae5a3e92c1de67d2
Kevin Burke <burke@shyp.com>
2016-04-21 16:02:28 -0700

    0.11

--- types.go
+++ types.go
@@ -6,7 +6,7 @@ import (
 	"encoding/json"
 )
 
-const VERSION = "0.10"
+const VERSION = "0.11"
 
 // A NullString is a String that may be null. It can be encoded or decoded from
 // JSON or the database.
